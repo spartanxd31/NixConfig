cf25a1868b62c4c6855508dcadb6d2b24432f2da
Domenic Marcelli <spartanxd31@vt.edu>
2026-04-27 14:43:57 -0400

    changed ssh things

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -6,9 +6,9 @@
       # Be sure to change it (using passwd) after rebooting!
       initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
+      # openssh.authorizedKeys.keys = [
+      #   # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      # ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = [
         "wheel"
